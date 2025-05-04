fd252ba1a91ba1984fcb785e89e415cab4c84d47
DanielJ56 <69170213+DanielJ56@users.noreply.github.com>
2025-05-03 21:17:51 -0600

    Update views.py

--- storedataapp/views.py
+++ storedataapp/views.py
@@ -17,19 +17,17 @@ def store_data(request):
     if (request.method == "POST"):
         data_body = request.body
         request_json = json.loads(data_body)
-        entry_person_id = request_json["person_id"]
-        entry_reserved_user_id = request_json["reserved_user_id"]
+
+
         entry_pronoun_id = request_json["pronoun_id"]
         entry_first_name = request_json["first_name"]
-        entry_middle_name = request_json["middle_name"]
+
         entry_last_name = request_json["last_name"]
         entry_birthdate = request_json["birthdate"]
         entry_nationality = request_json["nationality"]
-        entry_gender = request_json["gender"]
-        entry_passport_num_hash = request_json["passport_num_hash"]
-        entry_passport_num_salt = request_json["passport_num_salt"]
-        entry_bio = request_json["bio"]
-        entry_avatar_ref = request_json["avatar_ref"]
+
+
+
         
         person_entry = PERSON(person_id = entry_person_id, 
                               reserved_user_id =entry_reserved_user_id,
