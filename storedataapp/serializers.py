c694c1e46d29a856026f70d328afad513e8456fe
DanielJ56 <69170213+DanielJ56@users.noreply.github.com>
2025-05-03 21:18:39 -0600

    Update serializers.py

--- storedataapp/serializers.py
+++ storedataapp/serializers.py
@@ -4,7 +4,5 @@ from storedataapp.models import PERSON
 class PERSONSerializer(serializers.ModelSerializer):
     class Meta:
         model = PERSON
-        fields = ("person_id","reserved_user_id","pronoun_id","first_name",
-                 "middle_name","last_name","birthdate","nationality","gender",
-                 "passport_num_hash","passport_num_salt","passport_photo_ref",
-                 "bio","avatar_ref")
+        fields = ("person_id","reserved_user_id","first_name","last_name","birthdate","nationality","gender",
+                 "passport_num","passport_ghost")
